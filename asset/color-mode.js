fd2bcf853c5dfa4ca15c1829304f323fada7c57e
Arib Anwar <hiareeb@gmail.com>
2025-12-10 11:15:11 +0530

    Card-2

--- asset/color-mode.js
+++ asset/color-mode.js
@@ -1,36 +1,38 @@
 var toggleColorMode = function toggleColorMode(e) {
     var isLight = e.currentTarget.classList.contains("sun");
+    var newMode = isLight ? "light" : "dark";
     
     // Change color mode first
-    if (isLight) {
-      document.documentElement.setAttribute("color-mode", "light");
-      localStorage.setItem("color-mode", "light");
-      
-      // Add jump animation to the moon button that will become visible
-      setTimeout(function() {
-        var moonBtn = document.querySelector(".color-mode-btn.moon");
-        if (moonBtn) {
-          moonBtn.classList.add("jump");
-          setTimeout(function() {
-            moonBtn.classList.remove("jump");
-          }, 500);
-        }
-      }, 50);
-    } else {
-      document.documentElement.setAttribute("color-mode", "dark");
-      localStorage.setItem("color-mode", "dark");
-      
-      // Add jump animation to the sun button that will become visible
-      setTimeout(function() {
-        var sunBtn = document.querySelector(".color-mode-btn.sun");
-        if (sunBtn) {
-          sunBtn.classList.add("jump");
-          setTimeout(function() {
-            sunBtn.classList.remove("jump");
-          }, 500);
-        }
-      }, 50);
+    document.documentElement.setAttribute("color-mode", newMode);
+    localStorage.setItem("color-mode", newMode);
+    
+    // Send message to iframe to update its theme too
+    var chartIframe = document.getElementById("chart");
+    if (chartIframe && chartIframe.contentWindow) {
+      chartIframe.contentWindow.postMessage({ colorMode: newMode }, "*");
     }
+    
+    // Add jump animation to the moon button that will become visible
+    setTimeout(function() {
+      var moonBtn = document.querySelector(".color-mode-btn.moon");
+      if (moonBtn) {
+        moonBtn.classList.add("jump");
+        setTimeout(function() {
+          moonBtn.classList.remove("jump");
+        }, 500);
+      }
+    }, 50);
+    
+    // Add jump animation to the sun button that will become visible
+    setTimeout(function() {
+      var sunBtn = document.querySelector(".color-mode-btn.sun");
+      if (sunBtn) {
+        sunBtn.classList.add("jump");
+        setTimeout(function() {
+          sunBtn.classList.remove("jump");
+        }, 500);
+      }
+    }, 50);
   };
 
   // Get the buttons in the DOM
